ace9d2695118b6db232e26ab1efb86cf380aafff
Richie Cahill <richie@tmmworkshop.com>
2024-01-28 16:40:02 -0500

    added logging and new line to env_vars

--- docker_update.py
+++ docker_update.py
@@ -14,9 +14,13 @@ def create_env_file(env_var_data: dict[str, str], env_path: Path) -> None:
         env_var_data (dict[str, str]): dict of env vars
         env_path (Path): path to env file
     """
+    logging.info(f"Creating env file at path: {env_path}")
+
     env_vars = "\n".join([f"{key}={value}" for key, value in env_var_data.items()])
 
-    env_path.write_text(data=env_vars, encoding="utf-8", newline="\n")
+    logging.debug(f"{env_vars=}")
+
+    env_path.write_text(data=f"{env_vars}\n", encoding="utf-8")
 
 
 def run_command(command: str) -> tuple[str, int]:
@@ -32,14 +36,22 @@ def run_command(command: str) -> tuple[str, int]:
     return proses.stdout, proses.returncode
 
 
-def docker_compose_up() -> None:
-    """Docker compose up"""
-    path = "./jeeves-jr/docker-compose.yml"
+def docker_compose_up(path: str) -> None:
+    """Runs docker compose up
+
+    Args:
+        path (str): path to docker compose file
+    """
+    logging.info(f"Running docker compose up with path: {path}")
+
     output, returncode = run_command(f"docker compose -f {path} up --force-recreate --build -d")
+    logging.debug(f"docker compose up output: {output} returncode: {returncode} returncode: {returncode}")
 
     if returncode != 0:
         error = f"docker compose up failed with return code: {returncode} and output: {output}"
         raise ValueError(error)
+
+    logging.info("docker compose succeeded")
 
 
 def check_zfs(pool_name: str, data_set_name: str) -> None:
@@ -49,7 +61,10 @@ def check_zfs(pool_name: str, data_set_name: str) -> None:
         pool_name (str): zfs pool name
         data_set_name (str): zfs data set name
     """
+    logging.info(f"Checking if ZFS-{pool_name}-{data_set_name} is up")
+
     output, returncode = run_command(f"systemctl status ZFS-{pool_name}-{data_set_name}.mount")
+    logging.debug(f"systemctl status ZFS-{pool_name}-{data_set_name}.mount output: {output} returncode: {returncode}")
 
     if not re.compile(r"active \(mounted\)").search(output):
         error = f"ZFS-{pool_name}-{data_set_name} is not up with output: {output}"
@@ -58,6 +73,8 @@ def check_zfs(pool_name: str, data_set_name: str) -> None:
     if returncode != 0:
         error = f"systemctl status ZFS-{pool_name}-{data_set_name}.mount had returncode {returncode}: {output}"
         raise ValueError(error)
+
+    logging.info(f"ZFS-{pool_name}-{data_set_name} is up")
 
 
 def main() -> None:
@@ -68,17 +85,20 @@ def main() -> None:
         handlers=[logging.FileHandler("debug.log"), logging.StreamHandler()],
     )
 
+    logging.info("Starting docker update")
+
     check_zfs(pool_name="Main", data_set_name="Docker")
 
     create_env_file(
         env_var_data={"TUNNEL_TOKEN": environ["TUNNEL_TOKEN"]},
-        env_path=Path("/ZFS/Main/Docker/Docker/cloudflare_tunnel.env"),
+        env_path=Path("/ZFS/Main/Docker/Docker/jeeves-jr/cloudflare_tunnel.env"),
     )
-
 
     # TODO(Richie): argparser for machine name
 
-    # docker_compose_up()
+    # docker_compose_up(path = "./jeeves-jr/docker-compose.yml")
+
+    logging.info("docker update succeeded")
 
 
 if __name__ == "__main__":
