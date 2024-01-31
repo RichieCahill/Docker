5d365b8e5316c2c25b94c13030264b345ac57169
Richie Cahill <richie@tmmworkshop.com>
2024-01-30 19:08:09 -0500

    removed sandboxes dir

--- Sandboxes/Arch/User.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mypassword=$(cat /config/Secret/passwd)
-
-groupadd r2r0m0c0
-useradd -m -g r2r0m0c0 -G users -G wheel r2r0m0c0
-# chsh -s /bin/zsh r2r0m0c0
-echo "r2r0m0c0:$mypassword" | chpasswd
-
-
--- Sandboxes/Arch_Nvidea/arch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#setup time zone
-timedatectl set-timezone America/New_York
-systemctl enable systemd-timesyncd
-
-
-
